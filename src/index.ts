499ce6de32447655e508f6bc5639b6ea2726ff26
Callum Macdonald <git@callum-macdonald.com>
2019-11-05 17:17:22 +0100

    Skeleton graphql server.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-import { Settings, Actions } from "./data";
+import { GraphQLServer } from "graphql-yoga";
+
+// import { Settings, Actions } from "./data";
+
+const typeDefs = `
+type Lamp {
+  index: Int!
+  name: String!
+}
+type Query {
+  lamps: [Lamp]
+}
+`;
+
+const resolvers = {
+  Query: {
+    lamps: (root, args) => {
+      return [{ index: "1", name: "foo" }, { index: "2", name: "bar" }];
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+server.start(() => {
+  console.log("Server started on localhost:4000 #AUJVSy");
+});
