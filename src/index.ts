19fa504c709e7f8c30d7fd7f8298a60241b47f5e
Callum Macdonald <git@callum-macdonald.com>
2019-11-06 16:04:04 +0100

    Rename to lights.

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,121 @@
 import { GraphQLServer } from "graphql-yoga";
 
 // import { Settings, Actions } from "./data";
-import { getLamps } from "./utils";
+import { getLights, getHue } from "./utils";
+import { toColour } from "./toColour";
+import { startTransitionToColour } from "./actions";
 
 const typeDefs = `
-type LampState {
+type LightState {
   on: Boolean!
   "The colour mode this lamp is currently set to. Only for lamps which support colours."
   colormode: String
   reachable: Boolean!
 }
-type Lamp {
-  index: Int!
+
+type Light {
+  hueIndex: Int!
   name: String!
-  state: LampState!
+  state: LightState!
 }
+
 type Query {
-  lamps: [Lamp]
+  lights: [Light]
 }
-input SetColourInput {
-  "The Hue bridge index for this lamp"
-  index: Int!
+
+input GoToColourInput {
+  """
+  The Hue provided indexes of the lights to control.
+  """
+  hueIndexes: [Int!]
   "The colour (a name, like red, blue, etc)"
   colour: String!
+  "The time the transition should take"
+  timeMinutes: Int!
 }
-type SetColourResponse {
+type GoToColourResponse {
   """
-  This indicates that the request succeeded, but not that the Hue bridge
-  actually successfully processed the update.
+  This indicates that the request succeeded, and that the operation has
+  started.
   """
   success: Boolean
 }
+
+input GoToBrightnessInput {
+  """
+  The Hue provided indexes of the lights to control.
+  """
+  hueIndexes: [Int!]
+  """
+  The brightness this bulb should end up at, from 0 to 255.
+  NOTE: A brightness of 0 does not turn off the light.
+  """
+  brightness: Int!
+  "The time the transition should take"
+  timeMinutes: Int!
+}
+
+type GoToBrightnessResponse {
+  """
+  This indicates that the request succeeded, and that the operation has
+  started.
+  """
+  success: Boolean
+}
+
 type Mutation {
-  setColour(input: SetColourInput!): SetColourResponse
+  goToColour(input: GoToColourInput!): GoToColourResponse
+  goToBrightness(input: GoToBrightnessInput!): GoToBrightnessResponse
 }
 `;
 
-const resolvers = {
-  Query: {
-    lamps: getLamps
-  },
-  Mutation: {
-    setColour: async (root, args) => {
-      return {
-        success: true
-      };
-    }
-  }
+type GoToColourInputArgs = {
+  hueIndexes: number[];
+  colour: string;
+  timeMinutes: number;
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const makeResolvers = ({ hue }) => {
+  return {
+    Query: {
+      lights: getLights
+    },
+    Mutation: {
+      goToColour: async (root, args: { input: GoToColourInputArgs }) => {
+        const { hueIndexes, colour, timeMinutes } = args.input;
 
-server.start(() => {
-  console.log("Server started on localhost:4000 #AUJVSy");
-});
+        const results = await Promise.all(
+          hueIndexes.map(index => {
+            return startTransitionToColour({
+              index,
+              colour,
+              timeMinutes,
+              hue
+            });
+          })
+        );
+        return {
+          // Only return `true` if all operations were successful
+          success: results.find(result => !result) || true,
+          results
+        };
+      }
+    }
+  };
+};
+
+const start = async () => {
+  const hue = await getHue();
+
+  const resolvers = makeResolvers({ hue });
+
+  const server = new GraphQLServer({ typeDefs, resolvers });
+  server.start(() => {
+    console.log("Server started on localhost:4000 #AUJVSy");
+  });
+
+  // tick()
+  // hue.setColor(6, "00ff00");
+};
+
+start();
