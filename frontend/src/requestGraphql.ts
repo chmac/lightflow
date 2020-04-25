268049c3fa7b374dc6e709a606c523265b0cd2a3
Callum Macdonald <git@callum-macdonald.com>
2020-04-25 16:28:14 +0200

    Use localhost:4000/graphql on dev.

--- frontend/src/requestGraphql.ts
+++ frontend/src/requestGraphql.ts
@@ -1,7 +1,10 @@
 import { request } from "graphql-request";
 import { Variables } from "graphql-request/dist/src/types";
 
-const url = "/graphql";
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000/graphql"
+    : "/graphql";
 
 export const requestGraphql = (query: string, variables?: Variables) =>
   request(url, query, variables);
