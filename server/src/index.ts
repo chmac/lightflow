667726c601c875bbdabe8b606e142668e71e47fc
Callum Macdonald <git@callum-macdonald.com>
2020-06-19 23:55:18 +0200

    Wrap startup in error handling.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,15 @@ import { getHue } from "./utils";
 import { startServer } from "./graphqlSchema";
 
 const start = async () => {
-  const hue = await getHue();
+  try {
+    const hue = await getHue();
 
-  await startServer({ hue });
+    await startServer({ hue });
+  } catch (err) {
+    console.error("Startup error #XQN67n");
+    console.error(err);
+    process.exit();
+  }
 
   // tick()
   // hue.setColor(6, "00ff00");
