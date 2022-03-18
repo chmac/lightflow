5476d1e45cd47d8d4f106866f5bbc9bc87ecfaef
Callum Macdonald <git@callum-macdonald.com>
2022-03-18 11:08:11 +0100

    Refactor frontend to provide hue username.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { getHueUsername, setHueUsername } from "./requestGraphql";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+if (getHueUsername() === "") {
+  const HUE_USERNAME = globalThis.prompt(`Enter your hue username #ywPEVn`);
+  if (typeof HUE_USERNAME === "string" && HUE_USERNAME.length > 5) {
+    setHueUsername(HUE_USERNAME);
+  } else {
+    globalThis.alert(
+      `Error: This is not a valid hue username. ` +
+        `See the hue docs for more. #Cv3vtX\n\n` +
+        `https://developers.meethue.com/develop/get-started-2/`
+    );
+  }
+  globalThis.window.location.reload();
+} else {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- frontend/src/requestGraphql.ts
+++ frontend/src/requestGraphql.ts
@@ -1,12 +1,33 @@
-import { request } from "graphql-request";
+import { GraphQLClient } from "graphql-request";
 import { Variables } from "graphql-request/dist/src/types";
+
+const KEY = `HUE_USERNAME`;
+
+export const getHueUsername = () => {
+  const HUE_USERNAME = globalThis.localStorage.getItem(KEY);
+  if (typeof HUE_USERNAME === "string" && HUE_USERNAME.length > 0) {
+    return HUE_USERNAME;
+  }
+
+  return "";
+};
+
+export const setHueUsername = (HUE_USERNAME: string) => {
+  globalThis.localStorage.setItem(KEY, HUE_USERNAME);
+};
 
 const url =
   process.env.NODE_ENV === "development"
     ? "http://localhost:4000/graphql"
     : "/graphql";
 
+const client = new GraphQLClient(url, {
+  headers: {
+    Authorization: getHueUsername(),
+  },
+});
+
 export const requestGraphql = <T extends any>(
   query: string,
   variables?: Variables
-) => request<T>(url, query, variables);
+) => client.request<T>(query, variables);
