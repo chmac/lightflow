5fbe02358871a91093f60c77cb945e5a40ceb1cd
Callum Macdonald <git@callum-macdonald.com>
2022-03-18 11:51:00 +0100

    Echo commands before executing.

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$OS" = "Darwin" ]; then
+  echo docker build -t lightflow .
   docker build -t lightflow .
 else
+  echo sudo docker build -t lightflow .
   sudo docker build -t lightflow .
 fi
